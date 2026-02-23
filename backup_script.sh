c9f8036d832f38fe42237c484e98fae18a3695c6
Muhammad Fathy <mohamed192509@fci.bu.edu.eg>
2026-02-23 13:36:46 +0200

    Restucture the repo + adding and modify scripts

--- backup_script.sh
+++ backup_script.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/bash
 
-# Directory to backup (current directory)
+# Author: Muhammad Fathy Moheb
+# Created Date:
+# Modified Date: 23/2/2026
+
+# Discription: Archive all files in the current directory
+
+
 target_destination=$(pwd)
 
-# Get only the directory name (no full path) as file names not allowed to contain / (will couse errors)
 base_dir_name=$(basename "$target_destination")
 
-# Create backup filename with timestamp and .tar.gz extension
 backup_file_name="backup_of_${base_dir_name}_$(date +%d%m%Y_%H%M%S).tar.gz"
 
 echo "Backup Process Starts ...."
-# Create compressed archive of the target destination (current directory)
-tar -czvf "$backup_file_name" -C "$target_destination" .
+tar -cvf "$backup_file_name" -C "$target_destination" .
 
 echo "Backup created successfully: $backup_file_name"
 
+exit 0
