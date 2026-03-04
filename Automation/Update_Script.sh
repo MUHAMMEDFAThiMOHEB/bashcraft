a748bcbd704ee9d03b2be1dfe6eb4b4e536b20ce
Muhammad Fathy <mohamed192509@fci.bu.edu.eg>
2026-03-04 02:43:28 +0200

    modify update automation script

--- Automation/Update_Script.sh
+++ Automation/Update_Script.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# Author: Muhammad Fathy Moheb
+# Created Date: 3/3/2026
+# Modified Date: 4/3/2026
+#
+# Description:-
+# a Script to update all system packages and Homebrow packages in my machine
+#
+# Usage:-
+# automate my system Updates.
+#
 echo " "
 echo " ##### --- * Daily Script Start * --- ##### "
 echo ""
